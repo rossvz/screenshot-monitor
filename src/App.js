66d5bf9c232541b5343cb1058dc773ef06cea91e
Ross van Zyl <rossvz@getsellr.com>
2018-12-29 15:09:34 -0500

    :bug: (reloading) fixes issue with blank screen at the end of the list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import ms from 'milliseconds'
 import React, { useEffect, useState } from 'react'
 import './App.css'
-import ms from 'milliseconds'
-import ScreenshotGrid from './ScreenshotGrid'
 import { useDevices } from './hooks'
+import ScreenshotGrid from './ScreenshotGrid'
 
 const App = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
-	const {loading,devices} = useDevices(currentIndex)
-	useEffect(()=>{
-		setTimeout(()=>{
-			setCurrentIndex(currentIndex + 32 > devices.length ? 0 : currentIndex + 32)
-		},ms.seconds(20))
-	},[currentIndex, devices.length])
+  const { loading, devices } = useDevices(currentIndex)
+  useEffect(
+    () => {
+      setTimeout(() => {
+        setCurrentIndex(
+          currentIndex + 32 >= devices.length ? 0 : currentIndex + 32
+        )
+      }, ms.seconds(5))
+    },
+    [currentIndex, devices.length]
+  )
   return (
-    <div className='App'>
+    <div className="App">
+      {currentIndex}-{currentIndex + 32} /{devices.length} screenshots
       {loading && <h1>Loading...</h1>}
       {devices.length ? (
-        <ScreenshotGrid
-          devices={devices}
-          currentIndex={currentIndex}
-        />
+        <ScreenshotGrid devices={devices} currentIndex={currentIndex} />
       ) : null}
     </div>
   )
--- src/hooks.js
+++ src/hooks.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react'
 import { getDevices } from './helpers'
 
-export const useDevices = (currentIndex) => {
+export const useDevices = currentIndex => {
   const [loading, setLoading] = useState(false)
   const [devices, setDevices] = useState([])
-  useEffect(async () => {
-    if (!devices.length || currentIndex + 32 > devices.length) {
-      setLoading(true)
-      const data = await getDevices()
-      setDevices(data)
-      setLoading(false)
-    }
-  }, [currentIndex])
+  useEffect(
+    async () => {
+      if (!devices.length || currentIndex + 32 >= devices.length) {
+        setDevices([])
+        setLoading(true)
+        const data = await getDevices()
+        setDevices(data)
+        setLoading(false)
+      }
+    },
+    [currentIndex]
+  )
   return { loading, devices }
 }
